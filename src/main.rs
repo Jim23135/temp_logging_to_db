ae2dfe04496d3d7ba944319f2be01462e6009141
C <84656846+12e9881f53658734f7d0005e8f9091def068a65@users.noreply.github.com>
2024-07-24 21:14:26 -0700

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+use std::io::Read;
+use std::net::{TcpListener, TcpStream};
+use std::thread;
+
+fn handle_client(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+    
+    while match stream.read(&mut buffer) {
+        Ok(size) if size > 0 => {
+            let received_data = String::from_utf8_lossy(&buffer[..size]);
+            println!("Received: {}", received_data);
+            
+            true
+        },
+        _ => false,
+    } {}
+}
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("0.0.0.0:873").unwrap();
+
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                thread::spawn(move || handle_client(stream));
+            }
+            Err(e) => eprintln!("Failed to accept connection: {:?}", e),
+        }
+    }
 }
